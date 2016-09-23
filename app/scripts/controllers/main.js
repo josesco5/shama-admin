29be97eedd1acbb30776db2203f725001754f587
José Escorche <josesco5@gmail.com>
2016-09-22 23:11:50 -0400

    Initial commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name shamaAdminApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the shamaAdminApp
+ */
+angular.module('shamaAdminApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
