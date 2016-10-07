5d293394b073db61d0b085a66b44c6d63d94b6ec
Jose Escorche <josesco5@gmail.com>
2016-10-06 22:20:16 -0400

    Add translations and display messages on login

--- app/scripts/controllers/auth/auth.js
+++ app/scripts/controllers/auth/auth.js
@@ -8,7 +8,7 @@
  * Controller of the shamaAdminApp
  */
 angular.module('shamaAdminApp')
-  .controller('AuthCtrl', function ($scope, $state, auth, users) {
+  .controller('AuthCtrl', function ($scope, $state, $translate, auth, users, flash) {
     if (auth.loggedIn()) {
       $state.go('chats.list');
     }
@@ -36,11 +36,22 @@ angular.module('shamaAdminApp')
                 role: user.role,
                 enabled: user.enabled
               });
+              $translate('AUTH.MESSAGES.LOGIN_SUCCESS').then(function (msg) {
+                flash.showSuccess(msg);
+              });
               $state.go('chats.list');
             });
         }, function (response) {
           console.log('Login error with status: ' + response.status);
-          // ToDo: Display Error message
+          if (response.status == 401) {
+            $translate('AUTH.MESSAGES.LOGIN_UNAUTHORIZED_ERROR').then(function (msg) {
+              flash.showError(msg);
+            });
+          } else {
+            $translate('AUTH.MESSAGES.LOGIN_ERROR').then(function (msg) {
+              flash.showError(msg);
+            });
+          }
         });
     };
   });
